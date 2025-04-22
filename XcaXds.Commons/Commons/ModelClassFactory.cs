ac92b0a56d0f6b3023f6ced212a7c7cc70c7bbae
Vitaplex <128860039+Vitaplex@users.noreply.github.com>
2025-04-22 23:52:24 +0200

    mini

--- XcaXds.Commons/Commons/ModelClassFactory.cs
+++ XcaXds.Commons/Commons/ModelClassFactory.cs
@@ -12,9 +12,14 @@ public class CustomModelClassFactory : DefaultModelClassFactory
 {
     public override Type GetMessageClass(string theName, string theVersion, bool isExplicit)
     {
-        if (theVersion == "2.5" && theName == "QBP_Q22")
+        if (theVersion == "2.5" && theName == "QBP_Q21")
         {
-            return typeof(QBP_Q22); // Your custom class
+            // Field MSH-9-Message Type shall have all three components populated with a value.
+            // The first component shall have a value of QBP ;
+            // the second component shall have a value of Q22 . The third component it shall have a value of QBP_Q21.
+            // ( ...hrnngh )
+
+            return typeof(QBP_Q22); 
         }
 
         return base.GetMessageClass(theName, theVersion, isExplicit);
