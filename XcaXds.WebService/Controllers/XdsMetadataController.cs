223c902e5691384019a1bcfdb8d16795c0415e46
Clomby <assassin12@hotmail.no>
2025-04-26 03:00:04 +0200

    Refactor XdsMetadataController constructor to remove unused parameters and improve clarity

--- XcaXds.WebService/Controllers/XdsMetadataController.cs
+++ XcaXds.WebService/Controllers/XdsMetadataController.cs
@@ -11,34 +11,35 @@ namespace XcaXds.WebService.Controllers;
 [Route("api")]
 public class XdsMetadataController : ControllerBase
 {
-    private readonly ILogger<RegistryController> _logger;
-    private readonly XdsConfig _xdsConfig;
-    //private DocumentRegistry _documentRegistry;
-    //private readonly RegistryWrapper _registryWrapper;
+  private readonly ILogger<RegistryController> _logger;
+  private readonly XdsConfig _xdsConfig;
+  //private DocumentRegistry _documentRegistry;
+  //private readonly RegistryWrapper _registryWrapper;
 
-    public XdsMetadataController(ILogger<RegistryController> logger, XdsConfig xdsConfig, DocumentRegistry documentRegistry, RegistryWrapper registryWrapper)
-    {
-        _logger = logger;
-        _xdsConfig = xdsConfig;
-        //_documentRegistry = documentRegistry;
-        //_registryWrapper = registryWrapper;
+  public XdsMetadataController(ILogger<RegistryController> logger, XdsConfig xdsConfig)
+  {
+    // DocumentRegistry documentRegistry, RegistryWrapper registryWrapper
+    _logger = logger;
+    _xdsConfig = xdsConfig;
+    //_documentRegistry = documentRegistry;
+    //_registryWrapper = registryWrapper;
 
-        //_documentRegistry = _registryWrapper.GetDocumentRegistryContent() ?? new DocumentRegistry();
-    }
+    //_documentRegistry = _registryWrapper.GetDocumentRegistryContent() ?? new DocumentRegistry();
+  }
 
-    [Produces("application/json")]
-    [HttpGet("about/config")]
-    public async Task<IActionResult> GetXdsConfig()
-    {
-        return Ok(_xdsConfig);
-    }
+  [Produces("application/json")]
+  [HttpGet("about/config")]
+  public async Task<IActionResult> GetXdsConfig()
+  {
+    return Ok(_xdsConfig);
+  }
 
-    //[Produces("application/json")]
-    //[HttpGet("about/patients")]
-    //public async Task<IActionResult> GetPatientsInRegistry()
-    //{
-    //    var patientIds = _documentRegistry.RegistryObjectList.OfType<ExtrinsicObjectType>()
-    //        .Select(eo => eo.ExternalIdentifier.FirstOrDefault(ei => ei.IdentificationScheme == Constants.Xds.Uuids.DocumentEntry.PatientId).Value).ToList();
-    //    return Ok(patientIds);
-    //}
+  //[Produces("application/json")]
+  //[HttpGet("about/patients")]
+  //public async Task<IActionResult> GetPatientsInRegistry()
+  //{
+  //    var patientIds = _documentRegistry.RegistryObjectList.OfType<ExtrinsicObjectType>()
+  //        .Select(eo => eo.ExternalIdentifier.FirstOrDefault(ei => ei.IdentificationScheme == Constants.Xds.Uuids.DocumentEntry.PatientId).Value).ToList();
+  //    return Ok(patientIds);
+  //}
 }
