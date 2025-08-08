f82a483a170091296f7d49cd29901895a17f341c
Vitaplex <128860039+Vitaplex@users.noreply.github.com>
2025-08-08 08:44:44 +0200

    begin policy stuff

---
+++ XcaXds.Tests/UnitTests_PolicyAuthorization.cs
@@ -0,0 +1,17 @@
+﻿using Abc.Xacml.Context;
+using Abc.Xacml.Policy;
+using static XcaXds.Commons.Constants;
+
+namespace XcaXds.Tests;
+
+public class UnitTests_PolicyAuthorization
+{
+    [Fact]
+    public async Task Authorization_PolicyStuff()
+    {
+        var policy = new XacmlPolicy()
+        {
+            
+        };
+    }
+}
