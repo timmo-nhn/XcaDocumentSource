a25781f67095a8ff0c67179b4df76a30b279bc1c
timmo <tim.mortensen@nhn.no>
2025-10-02 14:32:52 +0200

    add power on seconds and startup datetime

--- XcaXds.WebService/Services/MonitoringStatusService.cs
+++ XcaXds.WebService/Services/MonitoringStatusService.cs
@@ -4,6 +4,7 @@ namespace XcaXds.WebService.Services;
 
 public class MonitoringStatusService
 {
+    public DateTimeOffset StartupTime { get; set; }
     public BoundedDictionary<string,long> ResponseTimes { get; set; }
     public MonitoringStatusService()
     {
