f70d49156b70442f572f7955bef2d37d3c00e15d
Tim Mortensen <tim.mortensen@nhn.no>
2025-12-01 15:12:24 +0100

    single line json logging

--- XcaXds.Commons/Serializers/SoapXmlSerializer.cs
+++ XcaXds.Commons/Serializers/SoapXmlSerializer.cs
@@ -3,7 +3,6 @@ using System.Xml;
 using System.Xml.Serialization;
 using XcaXds.Commons.Commons;
 using XcaXds.Commons.Extensions;
-using XcaXds.Commons.Models.Soap;
 
 namespace XcaXds.Commons.Serializers;
 
