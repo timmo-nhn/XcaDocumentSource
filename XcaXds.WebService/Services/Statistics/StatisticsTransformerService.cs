7f35146fe2a66aa4b4c596cd10bdd2d9f2acb324
timmo-nhn <tim.mortensen@nhn.no>
2026-05-12 11:36:55 +0200

    now?!

--- XcaXds.WebService/Services/Statistics/StatisticsTransformerService.cs
+++ XcaXds.WebService/Services/Statistics/StatisticsTransformerService.cs
@@ -1,9 +1,8 @@
-﻿using Hl7.Fhir.Model;
+﻿using System.Security.Cryptography;
+using System.Text;
+using Hl7.Fhir.Model;
 using Hl7.Fhir.Serialization;
 using Microsoft.IdentityModel.Tokens.Saml2;
-using Microsoft.OpenApi.MicrosoftExtensions;
-using System.Security.Cryptography;
-using System.Text;
 using XcaXds.Commons.Commons;
 using XcaXds.Commons.DataManipulators;
 using XcaXds.Commons.Extensions;
@@ -22,31 +21,33 @@ public class StatisticsTransformerService
     private readonly RegistryWrapper _registryWrapper;
     private readonly ApplicationConfig _appConfig;
 
-    public StatisticsTransformerService(ILogger<StatisticsTransformerService> logger, RegistryWrapper registryWrapper, ApplicationConfig appConfig)
+    public StatisticsTransformerService(ILogger<StatisticsTransformerService> logger, RegistryWrapper registryWrapper,
+        ApplicationConfig appConfig)
     {
         _logger = logger;
         _registryWrapper = registryWrapper;
         _appConfig = appConfig;
     }
 
-    public UserAccessEntry TransformToUserAccessEntry(StatisticsRequestAndFields inputFields)
+    public async Task<UserAccessEntry> TransformToUserAccessEntry(StatisticsRequestAndFields inputFields)
     {
         var userAccessEntry = new UserAccessEntry();
 
         switch (inputFields.RequestType)
         {
             case RequestAndFieldRequestType.SoapEnvelope:
-                userAccessEntry = GetUserAccessEntryFromSoapEnvelopeBasedRequest(inputFields);
+                userAccessEntry = await GetUserAccessEntryFromSoapEnvelopeBasedRequest(inputFields);
                 break;
 
             case RequestAndFieldRequestType.FhirProvideBundle:
-                userAccessEntry = GetUserAccessEntryFromFhirProvideBundleBasedRequest(inputFields);
+                userAccessEntry = await GetUserAccessEntryFromFhirProvideBundleBasedRequest(inputFields);
                 break;
 
             case RequestAndFieldRequestType.FhirUrlBasedRequest:
-                userAccessEntry = GetUserAccessEntryFromFhirUrlBasedRequest(inputFields);
+                userAccessEntry = await GetUserAccessEntryFromFhirUrlBasedRequest(inputFields);
                 break;
 
+            case RequestAndFieldRequestType.Unknown:
             default:
                 break;
         }
@@ -54,18 +55,20 @@ public class StatisticsTransformerService
         return userAccessEntry;
     }
 
-    private UserAccessEntry GetUserAccessEntryFromFhirProvideBundleBasedRequest(StatisticsRequestAndFields inputFields)
+    private async Task<UserAccessEntry> GetUserAccessEntryFromFhirProvideBundleBasedRequest(StatisticsRequestAndFields inputFields)
     {
         var jwt = JwtExtractor.ExtractJwt(inputFields.JwtToken, out _);
         var fhirparser = new FhirJsonDeserializer();
 
-        var fhirBundleRequest = fhirparser.TryDeserializeResource(inputFields.RequestBody, out var instance, out _) ? instance as Bundle: null;
-        var fhirBundleResponse = fhirparser.TryDeserializeResource(inputFields.RequestBody, out var instance2, out _) ? instance2: null;
+        var fhirBundleRequest = Hl7FhirExtensions.GetResourceFromStream(inputFields.RequestBody) as Bundle;
+        
+        var fhirBundleResponse = Hl7FhirExtensions.GetResourceFromStream(inputFields.RequestBody) as Bundle;
 
-        if (jwt == null && fhirBundleRequest == null) throw new InvalidOperationException("JWT or Fhir Bundle cannot be null.");
+        if (jwt == null && fhirBundleRequest == null)
+            throw new InvalidOperationException("JWT or Fhir Bundle cannot be null.");
 
         var samlToken = JwtToSamlTransformer.MapJsonWebTokenToSamlToken(jwt);
-        var userAccessEntry = GetUserAccessEntryFromFhirUrlBasedRequest(inputFields);
+        var userAccessEntry = await GetUserAccessEntryFromFhirUrlBasedRequest(inputFields);
 
         userAccessEntry.Issues = GetIssuesFromFhirResponse(fhirBundleResponse);
 
@@ -82,10 +85,11 @@ public class StatisticsTransformerService
             .OfType<OperationOutcome>()
             .FirstOrDefault() ?? responseOperationOutcome;
 
-        return operationOutcome?.Issue.Select(i => $"{i.Severity}: {i.Code} - {i.Diagnostics}").OfType<string>().ToArray();
+        return operationOutcome?.Issue.Select(i => $"{i.Severity}: {i.Code} - {i.Diagnostics}").OfType<string>()
+            .ToArray();
     }
 
-    private UserAccessEntry GetUserAccessEntryFromFhirUrlBasedRequest(StatisticsRequestAndFields inputFields)
+    private async Task<UserAccessEntry> GetUserAccessEntryFromFhirUrlBasedRequest(StatisticsRequestAndFields inputFields)
     {
         var jwt = JwtExtractor.ExtractJwt(inputFields.JwtToken, out _);
 
@@ -93,12 +97,15 @@ public class StatisticsTransformerService
 
         var samlToken = JwtToSamlTransformer.MapJsonWebTokenToSamlToken(jwt);
 
-        var statements = samlToken?.Assertion.Statements.OfType<Saml2AttributeStatement>().SelectMany(statement => statement.Attributes).ToList();
+        var statements = samlToken?.Assertion.Statements.OfType<Saml2AttributeStatement>()
+            .SelectMany(statement => statement.Attributes).ToList();
 
-        var subjectOrganization = GetSamlAttributeAsCodedValue(statements, "helseid://claims/client/claims/orgnr_parent");
+        var subjectOrganization =
+            GetSamlAttributeAsCodedValue(statements, "helseid://claims/client/claims/orgnr_parent");
         subjectOrganization?.CodeSystem ??= Constants.Oid.Brreg;
 
-        var subjectChildOrganization = GetSamlAttributeAsCodedValue(statements, "urn:oasis:names:tc:xspa:1.0:subject:child-organization");
+        var subjectChildOrganization =
+            GetSamlAttributeAsCodedValue(statements, "urn:oasis:names:tc:xspa:1.0:subject:child-organization");
         subjectChildOrganization?.CodeSystem ??= Constants.Oid.Brreg;
 
         return new UserAccessEntry()
@@ -112,49 +119,68 @@ public class StatisticsTransformerService
             SubjectOrganizationName = GetSamlAttributeAsString(statements, Constants.Saml.Attribute.Organization),
 
             SubjectChildOrganization = subjectChildOrganization,
-            SubjectChildOrganizationName = GetSamlAttributeAsString(statements, Constants.Saml.Attribute.TrustChildOrgName),
+            SubjectChildOrganizationName =
+                GetSamlAttributeAsString(statements, Constants.Saml.Attribute.TrustChildOrgName),
 
-            AccessBasis = GetSamlAttributeAsString(statements, Constants.Saml.Attribute.XuaAcp) ?? Constants.Oid.Saml.Acp.NullValue,
+            AccessBasis = GetSamlAttributeAsString(statements, Constants.Saml.Attribute.XuaAcp) ??
+                          Constants.Oid.Saml.Acp.NullValue,
 
             SourceHomeCommunityId = _appConfig.HomeCommunityId,
             SourceRepositoryUniqueId = _appConfig.RepositoryUniqueId,
             SourceHostName = _appConfig.HostName.Split("-xcadocumentsource").FirstOrDefault(),
 
-            DocumentConfidentialityCodes = inputFields.RelatedDocumentEntries?.SelectMany(d => d.ConfidentialityCode ?? []).OfType<CodedValue>()?.ToArray(),
+            DocumentConfidentialityCodes = inputFields.RelatedDocumentEntries
+                ?.SelectMany(d => d.ConfidentialityCode ?? []).OfType<CodedValue>()?.ToArray(),
             Endpoint = inputFields.Path,
             ResponseStatusCode = inputFields.StatusCode,
             AccessTime = inputFields.AccessTime,
             ElapsedTimeMillis = inputFields.ElapsedMilliseconds,
-
         };
     }
 
-    private UserAccessEntry GetUserAccessEntryFromSoapEnvelopeBasedRequest(StatisticsRequestAndFields inputFields)
+    private async Task<UserAccessEntry> GetUserAccessEntryFromSoapEnvelopeBasedRequest(StatisticsRequestAndFields inputFields)
     {
         var sxmls = new SoapXmlSerializer();
+        SoapEnvelope? soapEnvelopeRequest = null;
+        SoapEnvelope? soapEnvelopeResponse = null;
 
-        var soapEnvelopeRequest = sxmls.DeserializeXmlString<SoapEnvelope>(inputFields.RequestBody);
-        var soapEnvelopeResponse = sxmls.DeserializeXmlString<SoapEnvelope>(inputFields.ResponseBody);
+        if (inputFields.ContentType?.Split(";").FirstOrDefault() == Constants.MimeTypes.MultipartRelated &&
+            inputFields.RequestBody?.Length > 0 &&
+            inputFields.ResponseBody?.Length > 0)
+        {
+            soapEnvelopeRequest = await MultipartExtensions.ReadMultipartSoapMessage(inputFields.ContentType, inputFields.RequestBody);
+            soapEnvelopeResponse = await MultipartExtensions.ReadMultipartSoapMessage(inputFields.ContentType, inputFields.ResponseBody);
+        }
+        else
+        {
+            soapEnvelopeRequest = sxmls.DeserializeXmlString<SoapEnvelope>(inputFields.RequestBody);
+            soapEnvelopeResponse = sxmls.DeserializeXmlString<SoapEnvelope>(inputFields.ResponseBody);
+        }
 
         if (soapEnvelopeRequest == null) throw new InvalidOperationException("Soap request envelope cannot be null");
 
         var samlToken = SamlExtensions.ReadSamlToken(soapEnvelopeRequest.Header?.Security?.Assertion?.OuterXml);
 
-        var statements = samlToken?.Assertion.Statements.OfType<Saml2AttributeStatement>().SelectMany(statement => statement.Attributes).ToList();
+        var statements = samlToken?.Assertion.Statements.OfType<Saml2AttributeStatement>()
+            .SelectMany(statement => statement.Attributes).ToList();
 
         return new UserAccessEntry()
         {
             SessionId = soapEnvelopeRequest.Header?.MessageId,
             Issuer = samlToken?.Assertion.Issuer.Value,
             SubjectIdHash = GetSamlAttributeAsHashedString(statements, Constants.Saml.Attribute.ProviderIdentifier),
-            ResourceIdHash = GetSamlAttributeAsHashedString(statements, Constants.Saml.Attribute.ResourceId10, Constants.Saml.Attribute.ResourceId20),
+            ResourceIdHash = GetSamlAttributeAsHashedString(statements, Constants.Saml.Attribute.ResourceId10,
+                Constants.Saml.Attribute.ResourceId20),
 
             SubjectOrganization = GetSamlAttributeAsCodedValue(statements, Constants.Saml.Attribute.OrganizationId),
             SubjectOrganizationName = GetSamlAttributeAsString(statements, Constants.Saml.Attribute.Organization),
 
-            SubjectChildOrganization = GetSamlAttributeAsCodedValue(statements, Constants.Saml.Attribute.ChildOrganization),
-            SubjectChildOrganizationName = GetSamlAttributeAsString(statements, Constants.Saml.Attribute.TrustChildOrgName),
-            AccessBasis = GetSamlAttributeAsString(statements, Constants.Saml.Attribute.XuaAcp) ?? Constants.Oid.Saml.Acp.NullValue,
+            SubjectChildOrganization =
+                GetSamlAttributeAsCodedValue(statements, Constants.Saml.Attribute.ChildOrganization),
+            SubjectChildOrganizationName =
+                GetSamlAttributeAsString(statements, Constants.Saml.Attribute.TrustChildOrgName),
+            AccessBasis = GetSamlAttributeAsString(statements, Constants.Saml.Attribute.XuaAcp) ??
+                          Constants.Oid.Saml.Acp.NullValue,
 
             SourceHomeCommunityId = _appConfig.HomeCommunityId,
             SourceRepositoryUniqueId = _appConfig.RepositoryUniqueId,
@@ -172,7 +198,8 @@ public class StatisticsTransformerService
 
     private string[]? GetIssuesFromSoapEnvelope(SoapEnvelope? soapEnvelope)
     {
-        RegistryErrorType[] errors = [
+        RegistryErrorType[] errors =
+        [
             .. soapEnvelope?.Body.RetrieveDocumentSetResponse?.RegistryResponse?.RegistryErrorList?.RegistryError ?? [],
             ..soapEnvelope?.Body.AdhocQueryResponse?.RegistryErrorList?.RegistryError ?? [],
             ..soapEnvelope?.Body.RegistryResponse?.RegistryErrorList?.RegistryError ?? [],
@@ -186,18 +213,24 @@ public class StatisticsTransformerService
         return GetSamlAttributeAsCodedValue(statements, attributeNames)?.Code;
     }
 
-    private static CodedValue? GetSamlAttributeAsCodedValue(List<Saml2Attribute>? statements, params string[] attributeNames)
+    private static CodedValue? GetSamlAttributeAsCodedValue(List<Saml2Attribute>? statements,
+        params string[] attributeNames)
     {
-        var subjectOrganization = statements?.FirstOrDefault(s => s.Name.IsAnyOf(attributeNames))?.Values.FirstOrDefault();
+        var subjectOrganization =
+            statements?.FirstOrDefault(s => s.Name.IsAnyOf(attributeNames))?.Values.FirstOrDefault();
         return SamlExtensions.GetSamlAttributeValueAsCodedValue(subjectOrganization);
     }
 
-    private static string? GetSamlAttributeAsHashedString(List<Saml2Attribute>? statements, params string[] attributeNames)
+    private static string? GetSamlAttributeAsHashedString(List<Saml2Attribute>? statements,
+        params string[] attributeNames)
     {
-        var samlAttributeCoded = SamlExtensions.GetSamlAttributeValueAsCodedValue(statements?.FirstOrDefault(s => s.Name.IsAnyOf(attributeNames))?.Values.FirstOrDefault());
+        var samlAttributeCoded = SamlExtensions.GetSamlAttributeValueAsCodedValue(statements
+            ?.FirstOrDefault(s => s.Name.IsAnyOf(attributeNames))?.Values.FirstOrDefault());
         var samlStatement = (samlAttributeCoded?.Code + "^" + samlAttributeCoded?.CodeSystem).Trim('^');
 
-        return string.IsNullOrWhiteSpace(samlStatement) ? "Unknown" : Convert.ToHexString(MD5.HashData(Encoding.UTF8.GetBytes(samlStatement)));
+        return string.IsNullOrWhiteSpace(samlStatement)
+            ? "Unknown"
+            : Convert.ToHexString(MD5.HashData(Encoding.UTF8.GetBytes(samlStatement)));
     }
 
     private CodedValue[]? GetConfidentialityCodeFromProvidedBundle(Bundle fhirBundle)
@@ -226,5 +259,4 @@ public class StatisticsTransformerService
         var registryObject = _registryWrapper.GetSingleRegistryObjectAsDto(documentRequest.DocumentUniqueId);
         return (registryObject as DocumentEntryDto)?.ConfidentialityCode?.ToArray();
     }
-
 }
